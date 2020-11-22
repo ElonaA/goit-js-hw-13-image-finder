9eff625d38121b74d2d96c3714cde96bd33513b8
Elona Atroshchenko <ell.atroshchenko@gmail.com>
2020-11-23 01:05:23 +0200

    Добавила ссылку на прелоадер

--- src/js/refs.js
+++ src/js/refs.js
@@ -4,5 +4,6 @@ const refs = {
   lightBoxImage: document.querySelector("#lightbox"),
   loadMore: document.querySelector(".js-load-more"),
   clientHeight: document.documentElement.clientHeight,
+  preloader:document.querySelector(".preloader"),
 };
 export default  refs;
